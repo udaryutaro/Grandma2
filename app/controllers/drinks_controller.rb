8b3c59b2a45bfa217c3cb112965b151025dc6262
vagrant <vagrant@localhost.localdomain>
2021-01-10 11:10:10 +0000

    add drink_uptate,destroy

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -30,15 +30,19 @@ def new
   end
 
   def edit
-  	
+  	@drink = Drink.find(params[:id])
   end
 
   def update
-  	
+  	drink = Drink.find(params[:id])
+  	drink.update(drink_params)
+  	redirect_to drink_path(drink.id)
   end
 
   def destroy
-  	
+  	@drink = Drink.find(params[:id])
+  	@drink.destroy
+  	redirect_to drinks_path
   end
 
   private
