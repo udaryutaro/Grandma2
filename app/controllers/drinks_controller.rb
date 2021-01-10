61d5344bd0c4cb235e09991e1ee329d7d9d32248
vagrant <vagrant@localhost.localdomain>
2021-01-10 07:36:05 +0000

    add drink_index.html

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -7,6 +7,20 @@ def new
   	drink.save
   	redirect_to drinks_path
   end
+  def index
+  	@drink = Drink.all
+	@genres = Genre.all
+		# urlにcategory_id(params)がある場合
+        if params[:genre_id]
+		    # Categoryのデータベースのテーブルから一致するidを取得
+		    @genre = Genre.find(params[:genre_id])
+		    # category_idと紐づく投稿を取得
+		    @drinks = @genre.drinks.order(created_at: :desc).all.page(params[:page]).per(8)
+		else
+		    # 投稿すべてを取得
+	        @drinks = Drink.order(created_at: :desc).all.page(params[:page]).per(8)
+    	end
+  end
   private
   	def drink_params
   	params.require(:drink).permit(:drink_name, :image, :body, :genre_id )
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '794630aa485dab0f845dad070e8ad14536a15cae9a0b2a218bdd3bade2f1a5760320f90296dac5eb9bb68c67cf773c7462a1e5381eb4a47e77e1d1efc0c93f83'
