ed4e56f8dd09bdcb4ec6e7fb835c76e60c298696
vagrant <vagrant@localhost.localdomain>
2021-01-10 06:45:36 +0000

    add drinks_new.html

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,3 +1,14 @@
 class DrinksController < ApplicationController
-
+def new
+  	@drink = Drink.new
+  end
+  def create
+  	drink = Drink.new(drink_params)
+  	drink.save
+  	redirect_to drinks_path
+  end
+  private
+  	def drink_params
+  	params.require(:drink).permit(:drink_name, :image, :body, :genre_id )
+  	end
 end
